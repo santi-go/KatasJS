3918d3d52ac11ae5a5272ef9fa99633fcaabde0e
santi_go <santiago.gonzalez.erazo@gmail.com>
2017-11-18 20:25:59 +0100

    add basic constructors for Board & Cell

--- kataGameOfLife/spec/gameOfLifeSpec.js
+++ kataGameOfLife/spec/gameOfLifeSpec.js
@@ -4,8 +4,23 @@ describe("gameOfLife", function() {
 
   });
 
-  it("creates a board object with 2 properties",function(){
+  it("creates a board object with a columns property with value 3",function(){
     var board = new Board(3,3);
-    expect(board).toEqual({ columns : 3, rows : 3 });
+    expect(board.columns).toEqual(3);
+  });
+
+  it("renders a 3x3 board with all its cells alive",function(){
+    var board = new Board(3,3);
+    expect(board.renderInitBoard()).toEqual("***\n***\n***\n");
+  });
+
+  it("tells you if the created cell object is alive ",function(){
+    var cell = new Cell(true);
+    expect(cell.isAlive()).toEqual(true);
+  });
+
+  it("renders an alive cell",function(){
+    var cell = new Cell();
+    expect(cell.renderCell(cell.isAlive())).toEqual("*");
   });
 })
--- kataGameOfLife/src/gameOfLife.js
+++ kataGameOfLife/src/gameOfLife.js
@@ -1,4 +1,44 @@
-function Board(columns, rows){
+function liveOrDead() {
+  if(Math.random() > 0.5){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function Board(columns, rows) {
   this.columns = columns;
   this.rows = rows;
+}
+
+Board.prototype.renderInitBoard = function() {
+  var initBoard = [];
+  for(var i = 0; i < this.rows; i++){
+    var row = [];
+    for(var j = 0; j < this.columns; j++){
+      cell = new Cell(liveOrDead())
+      row.push(cell.renderCell(cell.isAlive()));
+    }
+    row = row.join("") + "\n";
+    initBoard.push(row);
+  }
+  return initBoard.join("");
+};
+
+
+
+function Cell() {
+  this.alive = liveOrDead();
+}
+
+Cell.prototype.isAlive = function() {
+    return this.alive;
+}
+
+Cell.prototype.renderCell = function(alive) {
+  if(alive === true) {
+    return "*";
+  } else {
+    return "-";
+  }
 }
