2bc99d26b7ef7bc71ded3ef372992b49ed00f92b
Santiago González Erazo <santiago.gonzalez.erazo@gmail.com>
2017-11-20 18:11:40 +0100

    finish and test cell.neighbourCounter

--- kataGameOfLife/spec/gameOfLifeSpec.js
+++ kataGameOfLife/spec/gameOfLifeSpec.js
@@ -1,38 +1,37 @@
 describe("gameOfLife", function() {
+  var board = new Board(3,3);
+  var boardArray = board.createBoardArray()
 
   beforeEach(function(){
 
   });
 
   it("creates a board object with a columns property with value 3",function(){
-    var board = new Board(3,3);
     expect(board.columns).toEqual(3);
   });
 
   it("renders a board in a form of a string",function(){
-    var board = new Board(30,30);
     expect(typeof board
-      .renderBoard(convertObjToStr(board.createBoardArray())))
+      .renderBoard(convertObjToStr(boardArray)))
       .toEqual("string");
   });
 
   it("creates a board array with random cells objects",function(){
-    var board = new Board(3,3);
     expect(typeof board.createBoardArray()).toEqual("object");
   });
 
-  it("tells you if the created cell object is alive ",function(){
-    var cell = new Cell(true);
-    expect(cell.isAlive()).toEqual(true);
-  });
-
-  it("tells you if the created cell object is alive ",function(){
-    var cell = new Cell(liveOrDead(), 2, 2);
-    expect(cell.whereIam()).toEqual([2, 2]);
+  it("creates a cell object with its ubication within the board",function(){
+    var cell = new Cell(true, 3, 5);
+    expect(cell.whereIam()).toEqual([3, 5]);
   });
 
   it("convert alive cells object into strings",function(){
     var board = new Board(3,3);
     expect(typeof convertObjToStr(board.createBoardArray())).toEqual("object");
+  });
+
+  it("counts the number of alive neighbours",function(){
+    board.renderBoard(convertObjToStr(boardArray))
+    expect(boardArray[2][2].neighbourCounter(boardArray)).toEqual(8);
   });
 })
--- kataGameOfLife/src/gameOfLife.js
+++ kataGameOfLife/src/gameOfLife.js
@@ -1,39 +1,8 @@
-function liveOrDead() {
-  if(Math.random() > 0.5){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function convertObjToStr(boardArray){
-  var boardStringArray = [];
-  for(var i = 0; i < boardArray.length; i++){
-    var row = [];
-    for(var j = 0 ;j < boardArray[i].length; j++){
-      var cell = boardArray[i][j].renderCell(boardArray[i][j].isAlive());
-      row.push(cell);
-    }
-    boardStringArray.push(row);
-  }
-  return boardStringArray;
-}
-
-function Board(columns, rows) {
+function Board (columns, rows) {
   this.columns = columns;
   this.rows = rows;
 }
 
-Board.prototype.renderBoard = function(boardArray) {
-  var boardString = "";
-  for(var i = 0; i < boardArray.length; i++){
-    var rowString = boardArray[i].join("") + "\n";
-    boardString += rowString;
-  }
-  console.log(boardString)
-  return boardString;
-};
-//diferencias entre initBoard y otros boards;
 Board.prototype.createBoardArray = function() {
   var boardArray = [];
   for(var i = 0; i < this.rows; i++){
@@ -46,6 +15,18 @@ Board.prototype.createBoardArray = function() {
   }
   return boardArray;
 };
+
+Board.prototype.renderBoard = function(boardArray) {
+  var boardString = "";
+  for(var i = 0; i < boardArray.length; i++){
+    var rowString = boardArray[i].join("") + "\n";
+    boardString += rowString;
+  }
+  console.log(boardString)
+  return boardString;
+};
+
+
 
 function Cell(alive, row, column) {
   this.alive = alive;
@@ -71,12 +52,118 @@ Cell.prototype.renderCell = function(alive) {
 
 Cell.prototype.neighbourCounter = function(boardArray) {
     var count = 0;
-    for(var i = this.row - 1; i < this.row + 1; i++){
-        for(var j = this.column - 1; j < this.column + 1; j++){
-            if(boardArray[i][j].isAlive() === true){
-              count++
-            }
+
+    if(this.row === 0 && this.column === 0){
+      for(var i = this.row; i <= this.row + 1; i++){
+        for(var j = this.column; j <= this.column + 1; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
         }
+      }
+    }
+
+    if(this.row === 0 && this.column < boardArray[0].length - 1){
+      for(var i = this.row; i <= this.row + 1; i++){
+        for(var j = this.column - 1; j <= this.column + 1; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row === 0 && this.column === boardArray[0].length - 1){
+      for(var i = this.row; i <= this.row + 1; i++){
+        for(var j = this.column - 1; j <= this.column; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row > 0 && this.row < boardArray.length -1 && this.column === 0){
+      for(var i = this.row - 1; i <= this.row + 1; i++){
+        for(var j = this.column; j <= this.column + 1; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row > 0 && this.row < boardArray[0].length - 1
+      && this.column > 0 && this.column < boardArray.length - 1){
+      for(var i = this.row - 1; i <= this.row + 1; i++){
+        for(var j = this.column - 1; j <= this.column + 1; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row > 0 && this.row < boardArray.length -1 && this.column === boardArray[0].length - 1){
+      for(var i = this.row - 1; i <= this.row + 1; i++){
+        for(var j = this.column - 1; j <= this.column; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row === boardArray.length -1 && this.column === 0){
+      for(var i = this.row - 1; i <= this.row; i++){
+        for(var j = this.column; j <= this.column + 1; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row === boardArray.length -1 && this.column > 0
+      && this.column < boardArray[0].length - 1){
+      for(var i = this.row - 1; i <= this.row; i++){
+        for(var j = this.column - 1; j <= this.column + 1; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
+    }
+
+    if(this.row === boardArray.length -1 && this.column === boardArray[0].length - 1){
+      for(var i = this.row - 1; i <= this.row; i++){
+        for(var j = this.column - 1; j <= this.column; j++){
+          if(boardArray[i][j].isAlive() === true){
+            count++;
+          }
+        }
+      }
     }
     return count;
+}
+
+function liveOrDead() {
+  if(Math.random() > 0.5){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function convertObjToStr(boardArray){
+  var boardStringArray = [];
+  for(var i = 0; i < boardArray.length; i++){
+    var row = [];
+    for(var j = 0 ;j < boardArray[i].length; j++){
+      var cell = boardArray[i][j].renderCell(boardArray[i][j].isAlive());
+      row.push(cell);
+    }
+    boardStringArray.push(row);
+  }
+  return boardStringArray;
 }
